2a3fb67f4d359aeba2700fa62e5878a2003900ff
Arthur Cech Francisco <arthurcech@gmail.com>
2023-01-14 10:48:06 -0300

    aula 10: banco de dados (postgresql)

---
+++ aula10/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const pgp = require("pg-promise");
+
+const app = express();
+app.use(express.json());
+app.use("/", express.static("./client"));
+
+const connection = pgp()(
+  "postgres://postgres:postgrespw@localhost:32768/personalfinance"
+);
+
+app.get("/api/lancamentos", async function (req, res) {
+  const lancamentos = await connection.query(
+    "select * from financas_pessoais.lancamento",
+    []
+  );
+  res.json(lancamentos);
+});
+
+app.post("/api/lancamentos", async function (req, res) {
+  const lancamento = req.body;
+  await connection.query(
+    "insert into financas_pessoais.lancamento (mes, categoria, tipo, valor) values ($1, $2, $3, $4)",
+    [lancamento.mes, lancamento.categoria, lancamento.tipo, lancamento.valor]
+  );
+  res.end();
+});
+
+app.listen(3000);
